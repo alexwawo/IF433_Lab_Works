49ed9e321cbe6f496a6d21a7306f6f66b75f411c
alexwawo <alex.wawo@gmail.com>
2026-01-28 15:42:20 +0700

    week01: refactor to use val and string templates

--- src/oop_001_johnthor/week01/GradingSystem.kt
+++ src/oop_001_johnthor/week01/GradingSystem.kt
@@ -1,9 +1,10 @@
 package oop_001_johnthor.week01 // Pastikan package sesuai
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    // REFACTOR: Gunakan val dan hapus tipe data eksplisit
+    val name = "John Thor"
+    val score = 80
 
-    // Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    // REFACTOR: String Template ($name)
+    println("Nama: $name, Nilai: $score")
 }
