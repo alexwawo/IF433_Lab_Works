66c5fea2cac78f29fd8ef1d0471eea765baedc1f
alexwawo <alex.wawo@gmail.com>
2026-01-28 15:41:43 +0700

    week01: initial setup and procedural code

---
+++ src/oop_001_johnthor/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_001_johnthor.week01 // Pastikan package sesuai
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
